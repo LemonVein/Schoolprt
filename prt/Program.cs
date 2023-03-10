ed75ed77412bf28a17577f4211ee4082eb4bc468
LemonVein <97579782+LemonVein@users.noreply.github.com>
2023-03-10 19:54:42 +0900

    Add files via upload

---
+++ prt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace prt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write"); Console.WriteLine("Write");
+            Console.WriteLine("Write");
+            Console.WriteLine("Write");
+            Console.WriteLine("Write");
+            Console.WriteLine("Write");
+        }
+    }
+}
